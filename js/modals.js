efb374a191b750abbddd139f335acfc6c43f0ee6
sss_xks <30371455+sssxks@users.noreply.github.com>
2018-10-12 18:53:06 +0800

    Create modals.js

---
+++ js/modals.js
@@ -0,0 +1,17 @@
+
+/*
+modals: [modal,modal]
+*/
+Vue.component('modals', {
+  props: ['modals'],
+  /*data: function () {
+    return {
+      count: 0
+    }
+  },*/
+  render: function (createElement) {
+    return createElement(
+      
+    )
+  },
+})
